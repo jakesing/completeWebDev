33a8c3e3675973255ea4b7b44cd1c3d3a2d84785
Jake <jacob.b.singer@gmail.com>
2020-01-18 15:10:28 -0500

    updated the flex and grid (retail card thing), plus gradient thing

--- Section 12/background generator/script.js
+++ Section 12/background generator/script.js
@@ -2,13 +2,23 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var root = document.documentElement;
+// var initialvalue1 = getComputedStyle(root).getPropertyValue('--color1');
+// var initialvalue2 = getComputedStyle(root).getPropertyValue('--color2');
 
+//set initial value for colors
+// color1.value = getComputedStyle(document.documentElement).getPropertyValue('--color1');
+color1.value = getComputedStyle(root).getPropertyValue('--color1').slice(1,8);
+color2.value = getComputedStyle(root).getPropertyValue('--color2').slice(1,8);
+
+css.innerText = getComputedStyle(body).backgroundImage;
 
 //define function to change h3 text
 function changeText(){
 	css.innerText = body.style.background;	
 }
 
+// changeText();
 
 //define function to change the background to current value of color, and change text
 function changeBackground(){
@@ -20,12 +30,10 @@ function changeBackground(){
 	changeText();
 }
 
-
-//loop over colors to add event listener
-colors = [color1, color2]
-
-for(i in colors){
-	colors[i].addEventListener("input", changeBackground)
-}
+//set new background;
+color1.addEventListener("input", changeBackground);
+// color1.addEventListener("input", changeText);
+color2.addEventListener("input", changeBackground);
+// color2.addEventListener("input", changeText);
 
 
